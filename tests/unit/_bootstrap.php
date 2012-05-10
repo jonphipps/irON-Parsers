84e0cb54d68e52185479c6b38ca4f102759fd770
Jon Phipps <jphipps@madcreek.com>
2012-05-10 11:10:41 -0400

    Add autoloader to bootstrap

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -4,3 +4,7 @@
 
   //require_once //__DIR__ . '/../../config.xml';
   require_once __DIR__ . "/../../commON/CommonParser.php";
+
+  require_once __DIR__ . '/../../EasyCSV/lib/EasyCSV/ClassLoader.php';
+  $classLoader = new \EasyCSV\ClassLoader('EasyCSV', __DIR__ . '/../../EasyCSV/lib');
+  $classLoader->register();
