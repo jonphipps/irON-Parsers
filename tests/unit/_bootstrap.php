285a9d57f89fe5991d79dbdb1368dc6636c56fc8
Jon Phipps <jphipps@madcreek.com>
2012-05-11 19:05:29 -0400

    adjusted loaders in test bootstrap

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -3,8 +3,13 @@
   require_once 'PHPUnit/Framework/Assert/Functions.php';
 
   //require_once //__DIR__ . '/../../config.xml';
-  require_once __DIR__ . "/../../commON/CommonParser.php";
+  require_once __DIR__ . "/../../lib/IronParsers/commON/CommonParser.php";
+
+  require_once __DIR__ . '/../../lib/IronParsers/ClassLoader.php';
+  $classLoader = new \IronParsers\ClassLoader('IronParsers');
+  $classLoader->register();
 
   require_once __DIR__ . '/../../EasyCSV/lib/EasyCSV/ClassLoader.php';
   $classLoader = new \EasyCSV\ClassLoader('EasyCSV', __DIR__ . '/../../EasyCSV/lib');
   $classLoader->register();
+
