9a1ad68a9ef74fc0fdc19c1c51b271b05de1a055
Jon Phipps <jphipps@madcreek.com>
2012-05-11 23:56:08 -0400

    still trying to get the class loaders right

--- tests/IronParsers/Tests/bootstrap.php
+++ tests/IronParsers/Tests/bootstrap.php
@@ -4,15 +4,15 @@ namespace IronParsers\Tests;
 
 error_reporting(E_ALL | E_STRICT);
 
-require_once 'PHPUnit/Framework/Assert/Functions.php';
-require_once 'PHPUnit/TextUI/TestRunner.php';
+//require_once 'PHPUnit/Framework/Assert/Functions.php';
+//require_once 'PHPUnit/TextUI/TestRunner.php';
 require_once __DIR__ . '/../../../lib/IronParsers/ClassLoader.php';
 
 $classLoader = new \IronParsers\ClassLoader('IronParsers');
 $classLoader->register();
 
-//$classLoader = new \EasyCSV\ClassLoader('EasyCSV');
-//$classLoader->register();
+$classLoader = new \IronParsers\ClassLoader('EasyCSV', __DIR__ . '/../../../EasyCSV/lib');
+$classLoader->register();
 
 set_include_path(
     __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'lib'
