8336c19f1a44662a03f73f7ce04e2cf6aa54d6fd
Jon Phipps <jphipps@madcreek.com>
2012-05-13 13:16:21 -0400

    Yeah, I moved things around again

---
+++ tests/unit/_bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+namespace IronParsers\Tests;
+
+error_reporting(E_ALL | E_STRICT);
+
+require_once __DIR__ . '/../../lib/IronParsers/ClassLoader.php';
+
+$classLoader = new \IronParsers\ClassLoader('IronParsers');
+$classLoader->register();
+
+$classLoader = new \IronParsers\ClassLoader('EasyCSV', __DIR__ . '/../../EasyCSV/lib');
+$classLoader->register();
+
+set_include_path(
+	__DIR__ . DIRECTORY_SEPARATOR . 
+	'..' . DIRECTORY_SEPARATOR . 
+	'..' . DIRECTORY_SEPARATOR . 
+	'lib'. PATH_SEPARATOR .
+    get_include_path()
+	);
